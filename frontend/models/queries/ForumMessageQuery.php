74030d54363ba341ea658f3f262d62848d4e8e46
Ihor Boiko <igosja@ukr.net>
2020-11-09 14:56:18 +0200

    forum block on index page

--- frontend/models/queries/ForumMessageQuery.php
+++ frontend/models/queries/ForumMessageQuery.php
@@ -19,19 +19,15 @@ class ForumMessageQuery
          * @var ForumMessage[] $forumMessages
          */
         $forumMessages = ForumMessage::find()
-            ->select(
-                [
-                    'forum_message.*',
-                    'id' => 'MAX(forum_message.id)',
-                    'date' => 'MAX(forum_message.date)',
-                ]
-            )
+            ->select([
+                'forum_message.*',
+                'id' => 'MAX(forum_message.id)',
+                'date' => 'MAX(forum_message.date)',
+            ])
             ->joinWith(['forumTheme.forumGroup'])
-            ->andWhere(
-                [
-                    'forum_group.country_id' => 0
-                ]
-            )
+            ->andWhere([
+                'forum_group.federation_id' => null
+            ])
             ->groupBy(['forum_theme_id'])
             ->orderBy(['forum_message.id' => SORT_DESC])
             ->limit(10)
