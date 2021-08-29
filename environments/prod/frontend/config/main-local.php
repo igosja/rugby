6b0d50edbf932b614d324f37ba931ccce5ea3d51
igosja <igosja@ukr.net>
2021-08-29 18:13:21 +0300

    https

--- environments/prod/frontend/config/main-local.php
+++ environments/prod/frontend/config/main-local.php
@@ -9,4 +9,12 @@ return [
             'cookieValidationKey' => '',
         ],
     ],
+    'on beforeRequest' => static function () {
+        if (!Yii::$app->request->isSecureConnection) {
+            $url = Yii::$app->request->getAbsoluteUrl();
+            $url = str_replace('http:', 'https:', $url);
+            Yii::$app->getResponse()->redirect($url);
+            Yii::$app->end();
+        }
+    },
 ];
