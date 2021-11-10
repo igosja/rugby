172987bd5d770ada973de6a994296787ee5e3aed
igosja <igosja@ukr.net>
2021-11-10 18:42:12 +0200

    admin moderation

---
+++ backend/views/moderation/news-comment-update.php
@@ -0,0 +1,30 @@
+<?php
+
+// TODO refactor
+
+use yii\bootstrap\ActiveForm;
+use yii\helpers\Html;
+
+/**
+ * @var \common\models\db\NewsComment $model
+ */
+
+?>
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+        <h3 class="page-header"><?= Html::encode($this->title) ?></h3>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <?php $form = ActiveForm::begin() ?>
+        <?= $form->field($model, 'text')->textarea() ?>
+        <div class="form-group">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+                <?= Html::submitButton('Save', ['class' => 'btn btn-default']) ?>
+            </div>
+        </div>
+        <?php ActiveForm::end() ?>
+    </div>
+</div>
+<?= $this->render('_reasons') ?>
