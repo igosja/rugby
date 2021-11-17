c91f60ed350c4e3ead92d007d8924045c3b59c57
igosja <igosja@ukr.net>
2021-11-17 17:03:32 +0200

    password

--- backend/views/generate/password.php
+++ backend/views/generate/password.php
@@ -6,12 +6,27 @@
  * @var string $password
  */
 
+use yii\helpers\Html;
+
 ?>
 
 <div class="site-index">
     <div class="row">
         <div class="col-lg-12 text-center">
             <h1 class="page-header"><?= $password ?></h1>
+            <?= Html::beginForm(['password'], 'get') ?>
+            <div class="form-group">
+                <?= Html::label('Length', null, ['class' => 'control-label']) ?>
+                <?= Html::textInput('l', Yii::$app->request->get('l', 10), ['class' => 'form-control']) ?>
+            </div>
+            <div class="form-group">
+                <?= Html::label('Special', null, ['class' => 'control-label']) ?>
+                <?= Html::checkbox('s', Yii::$app->request->get('s')) ?>
+            </div>
+            <div class="form-group">
+                <?= Html::submitButton('Submit', ['class' => ['btn', 'btn-default']]) ?>
+            </div>
+            <?= Html::endForm() ?>
         </div>
     </div>
 </div>
